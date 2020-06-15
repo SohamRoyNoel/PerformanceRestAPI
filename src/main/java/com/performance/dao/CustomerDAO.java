5979726b6c920671bac3a8f7723bae879ebedca5
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-06-15 13:19:12 +0530

    Rest Api Performance

---
+++ src/main/java/com/performance/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.performance.dao;
+
+import java.util.List;
+
+import com.performance.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
